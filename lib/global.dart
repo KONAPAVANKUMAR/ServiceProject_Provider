6a8b60da587ab635fe8f9a6c97e5cd6aa3675f18
PAVAN KUMAR KONA <konapavankumar.pk@gmail.com>
2021-11-14 20:44:11 +0530

    endpointdomain changed to localhost

--- lib/global.dart
+++ lib/global.dart
@@ -1,5 +1,5 @@
-// const endpointDomain = "http://127.0.0.1:8000/";
-const endpointDomain = "http://192.168.0.103:8000/";
+const endpointDomain = "http://127.0.0.1:8000/";
+// const endpointDomain = "http://192.168.0.103:8000/";
 const createProviderEndpoint = endpointDomain + 'users/create/serviceprovider';
 const authenticateProviderEndpoint =
     endpointDomain + 'users/authenticate/serviceprovider';
